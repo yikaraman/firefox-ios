bc475d119d5dc229b919313de9ab18d29a485f83
James Hugman <james@hugman.tv>
2017-10-11 23:13:27 +0100

    Bug 1407710, 1407707, 1407709, 1407702 — Add ScreenActions, ScreenState and UserState to ScreenGraph.
    https://github.com/mozilla-mobile/firefox-ios/issues/3272
    
    [ba0457f] Bug 1407710 — Add better tidy up of the backstack.
    [e420368] Bug 1407710 — Uncontroversial cleanup
    [baa1fcc] Bug 1407710 — Migrate to new onEnter methods.
    [bde8f5b] Bug 1407710 — Add navigator.plan()
    [61709d2] Bug 1407710 — Split onEnter into onEnter and onEnterWaitFor.
    
    This lets us add a conditional waitFor.
    
    screenState.onEnterWaitFor(element:, if: "")
    [04cc405] Bug 1407710 — Reduce visibility of test identifiers
    
    This was causing clashes with FxScreenGraph.
    [5631de4] Bug 1407710 — Add back() method, press() method, synchronizeWithUserState() method
    [a2f9332] Bug 1407710 — Add extra methods to expose new actions and conditional edges to all gestures.
    
    Improve error reporting.
    [c03a87e] Bug 1407707 — Add conditional edges to the screen graph.
    
    This lets us define edges (including actions) as conditional on the current userstate.
    
    If the userState changes (even mid `goto`), the conditional edges are re-evaluated, and the navigator is re-routed.
    
    Unfortunately, this commit adds mutable state to the graph, which means that there is now a one-to-one navigator-to-screengraph mapping (before you could have multiple navigators from the same instance of the ScreenGraph. This is almost certainly not worth worrying about.
    [e73fc54] Bug 1407706 — More testing of actions
    [9bd748e] Bug 1407706 — More testing of actions
    [904c492] Bug 1407706 — Add toggleOn and toggleOff actions.
    
    These depend on the userState being up to date.
    [868d2e5] Bug 1407706 — Move enter and leave into its own private extension.
    [13bdbd4] Bug 1407706 — Ensure actions always end in screen states.
    [327f9b5] Bug 1407706 — Actions can be added from within screen states.
    [a012a0e] Bug 1407706 — Refactor navigator.goto() into a enter and leave methods.
    
    This also changes currentScene to be using the new generalization of GraphNode.
    [059a011] Bug 1407706 — Start ScreenActionNodes
    
    Refactor ScreenGraphNode to ScreenStateNode and GraphNode.
    [6f994c0] Bug 1407709 — Add predicate tests for entry criteria into nodes.
    [dc3063e] Bug 1407702 — Add onEnter user state recorders.
    
    Unify how we look at the state of the app.
    [3558577] Bug 1407702 — Add failing tests for onEnter state changes
    [84cbccf] Bug 1407702 — Add UserState to ScreenGraph.
    
    This is the basis of adding actions and predicates to the screengraph. As neither of these exist, this is not very useful.

---
+++ XCUITests/ScreenGraphTest.swift
@@ -0,0 +1,236 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import XCTest
+
+class ScreenGraphTest: XCTestCase {
+    var navigator: Navigator<TestUserState>!
+    var app: XCUIApplication!
+
+    override func setUp() {
+        app = XCUIApplication()
+        navigator = createTestGraph(for: self, with: app).navigator()
+        app.terminate()
+        restart(app, args: [LaunchArguments.ClearProfile, LaunchArguments.SkipIntro])
+    }
+
+    func restart(_ app: XCUIApplication, args: [String] = []) {
+        XCUIDevice.shared().press(.home)
+        var launchArguments = [LaunchArguments.Test]
+        args.forEach { arg in
+            launchArguments.append(arg)
+        }
+        app.launchArguments = launchArguments
+        app.activate()
+    }
+}
+
+extension ScreenGraphTest {
+    func testUserStateChanges() {
+        XCTAssertNil(navigator.userState.url, "Current url is empty")
+        navigator.goto(BrowserTab)
+
+        XCTAssertTrue(navigator.userState.url?.starts(with: "support.mozilla.org") ?? false, "Current url recorded by from the url bar")
+    }
+
+    func testBackStack() {
+        // We'll go through the browser tab, through the menu.
+        navigator.goto(SettingsScreen)
+        // Going back, there is no explicit way back to the browser tab,
+        // and the menu will have dismissed. We should be detecting the existence of
+        // elements as we go through each screen state, so if there are errors, they'll be
+        // reported in the graph below.
+        navigator.goto(BrowserTab)
+    }
+
+    func testSimpleToggleAction() {
+        // Switch night mode on, by toggling.
+        navigator.performAction(TestActions.ToggleNightMode)
+        XCTAssertTrue(navigator.userState.nightMode)
+        XCTAssertEqual(navigator.screenState, BrowserTab)
+
+        // Nothing should happen here, because night mode is already on.
+        navigator.toggleOn(navigator.userState.nightMode, withAction: TestActions.ToggleNightMode)
+        XCTAssertTrue(navigator.userState.nightMode)
+        XCTAssertEqual(navigator.screenState, BrowserTab)
+
+        // Switch night mode off.
+        navigator.toggleOff(navigator.userState.nightMode, withAction: TestActions.ToggleNightMode)
+        XCTAssertFalse(navigator.userState.nightMode)
+        XCTAssertEqual(navigator.screenState, BrowserTab)
+    }
+
+    func testChainedActionPerf1() {
+        let navigator = self.navigator!
+        measure {
+            navigator.userState.url = defaultURL
+            navigator.performAction(TestActions.LoadURLByPasting)
+            XCTAssertEqual(navigator.screenState, BrowserTab)
+        }
+    }
+
+    func testChainedActionPerf2() {
+        let navigator = self.navigator!
+        measure {
+            navigator.userState.url = defaultURL
+            navigator.performAction(TestActions.LoadURLByTyping)
+            XCTAssertEqual(navigator.screenState, BrowserTab)
+        }
+
+        navigator.userState.url = defaultURL
+        navigator.performAction(TestActions.LoadURL)
+        XCTAssertEqual(navigator.screenState, BrowserTab)
+    }
+
+    func testConditionalEdgesSimple() {
+        XCTAssertTrue(navigator.can(goto: PasscodeSettingsOff))
+        XCTAssertFalse(navigator.can(goto: PasscodeSettingsOn))
+        navigator.goto(PasscodeSettingsOff)
+        XCTAssertEqual(navigator.screenState, PasscodeSettingsOff)
+    }
+
+    func testConditionalEdgesRerouting() {
+        // The navigator should dynamically reroute to the target screen
+        // if the userState changes.
+        // This test adds to the graph a passcode setting screen. In that screen,
+        // there is a noop action that fatalErrors if it is taken.
+        //
+        let map = createTestGraph(for: self, with: app)
+
+        func typePasscode(_ passCode: String) {
+            passCode.forEach { char in
+                app.keys["\(char)"].tap()
+            }
+        }
+
+        map.addScreenState(SetPasscodeScreen) { screenState in
+            // This is a silly way to organize things here,
+            // and is an artifical way to show that the navigator is re-routing midway through
+            // a goto.
+            screenState.onEnter() { userState in
+                typePasscode(userState.newPasscode)
+                typePasscode(userState.newPasscode)
+                userState.passcode = userState.newPasscode
+            }
+
+            screenState.noop(forAction: "FatalError", transitionTo: PasscodeSettingsOn, if: "passcode == nil") { _ in fatalError() }
+            screenState.noop(forAction: "Very", "Long", "Path", "Of", "Actions", transitionTo: PasscodeSettingsOn, if: "passcode != nil") { _ in }
+        }
+
+        navigator = map.navigator()
+
+        XCTAssertTrue(navigator.can(goto: PasscodeSettingsOn))
+        XCTAssertTrue(navigator.can(goto: PasscodeSettingsOff))
+        XCTAssertTrue(navigator.can(goto: "FatalError"))
+        navigator.goto(PasscodeSettingsOn)
+        XCTAssertTrue(navigator.can(goto: PasscodeSettingsOn))
+        XCTAssertFalse(navigator.can(goto: PasscodeSettingsOff))
+        XCTAssertFalse(navigator.can(goto: "FatalError"))
+
+        XCTAssertEqual(navigator.screenState, PasscodeSettingsOn)
+    }
+}
+
+
+private let defaultURL = "https://example.com"
+class TestUserState: UserState {
+    required init() {
+        super.init()
+        initialScreenState = FirstRun
+    }
+
+    var url: String? = nil
+    var nightMode = false
+    var passcode: String? = nil
+    var newPasscode: String = "1111"
+}
+
+let PasscodeSettingsOn = "PasscodeSettingsOn"
+let PasscodeSettingsOff = "PasscodeSettingsOff"
+
+fileprivate class TestActions {
+    static let ToggleNightMode = "menu-NightMode"
+    static let LoadURL = "LoadURL"
+    static let LoadURLByTyping = "LoadURLByTyping"
+    static let LoadURLByPasting = "LoadURLByPasting"
+}
+
+fileprivate func createTestGraph(for test: XCTestCase, with app: XCUIApplication) -> ScreenGraph<TestUserState> {
+    let map = ScreenGraph(for: test, with: TestUserState.self)
+
+    map.addScreenState(FirstRun) { screenState in
+        screenState.noop(to: BrowserTab)
+        screenState.tap(app.textFields["url"], to: URLBarOpen)
+    }
+
+    map.addScreenState(BrowserTab) { screenState in
+        screenState.onEnterWaitFor("exists != true", element: app.progressIndicators.element(boundBy: 0))
+        screenState.onEnter { userState in
+            userState.url = app.textFields["url"].value as? String
+        }
+
+        screenState.tap(app.buttons["TabToolbar.menuButton"], to: BrowserTabMenu)
+        screenState.tap(app.textFields["url"], to: URLBarOpen)
+
+        screenState.gesture(forAction: TestActions.LoadURLByPasting, TestActions.LoadURL) { userState in
+            UIPasteboard.general.string = userState.url ?? defaultURL
+            app.textFields["url"].press(forDuration: 1.0)
+            app.sheets.element(boundBy: 0).buttons.element(boundBy: 0).tap()
+        }
+    }
+
+    map.addScreenState(URLBarOpen) { screenState in
+        screenState.gesture(forAction: TestActions.LoadURLByTyping, TestActions.LoadURL) { userState in
+            let urlString = userState.url ?? defaultURL
+            app.textFields["address"].typeText("\(urlString)\r")
+        }
+    }
+
+    map.addScreenAction(TestActions.LoadURL, transitionTo: BrowserTab) { userState in
+        // NOP
+    }
+
+    map.addScreenState(BrowserTabMenu) { screenState in
+        screenState.dismissOnUse = true
+        screenState.onEnterWaitFor(element: app.tables["Context Menu"])
+        screenState.tap(app.tables.cells["Settings"], to: SettingsScreen)
+
+        screenState.tap(app.cells["menu-NightMode"], forAction: TestActions.ToggleNightMode) { userState in
+            userState.nightMode = !userState.nightMode
+        }
+
+        screenState.backAction = {
+            app.buttons["PhotonMenu.cancel"].tap()
+        }
+    }
+
+    let navigationControllerBackAction = {
+        app.navigationBars.element(boundBy: 0).buttons.element(boundBy: 0).tap()
+    }
+
+    map.addScreenState(SettingsScreen) { screenState in
+        let table = app.tables["AppSettingsTableViewController.tableView"]
+        screenState.onEnterWaitFor(element: table)
+
+        screenState.tap(table.cells["TouchIDPasscode"], to: PasscodeSettingsOff, if: "passcode == nil")
+        screenState.tap(table.cells["TouchIDPasscode"], to: PasscodeSettingsOn, if: "passcode != nil")
+
+        screenState.backAction = navigationControllerBackAction
+    }
+
+    map.addScreenState(PasscodeSettingsOn) { screenState in
+        screenState.backAction = navigationControllerBackAction
+    }
+
+    map.addScreenState(PasscodeSettingsOff) { screenState in
+        screenState.tap(app.staticTexts["Turn Passcode On"], to: SetPasscodeScreen)
+        screenState.backAction = navigationControllerBackAction
+    }
+
+    map.addScreenState(SetPasscodeScreen) { screenState in
+        screenState.backAction = navigationControllerBackAction
+    }
+
+    return map
+}
